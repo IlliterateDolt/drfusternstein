db7266acc16c82aa0d1620b412c7bdce4131617b
IlliterateDolt <68133548+IlliterateDolt@users.noreply.github.com>
2020-12-17 19:42:31 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+get_header();
+?>
+
+
+	<div class="inner_content">
+		<?php 
+		echo 'Hello World!';
+
+		?>
+		<br/>
+		<button onclick="javascript:window.print()">Print Success Page</button>
+	</div>
+
+
+	
+
+
+<?php
+get_footer();
