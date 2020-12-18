03531c001bf702425bb8df15310dcffc862b0005
IlliterateDolt <68133548+IlliterateDolt@users.noreply.github.com>
2020-12-17 19:41:48 -0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,20 +1,54 @@
-<?php
-get_header();
-?>
+<?php 
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ */
+  ?>
+
+	</div><!-- End the content -->
+
+	<!--Start of center social media icons-->
+
+	<div class="row">
+		<div class="socialcolumn">
+			<h3>Music</h3>
 
 
-	<div class="inner_content">
-		<?php 
-		echo 'Hello World!';
+			<div class="externallinks">
+				<a href="https://play.spotify.com/artist/0a2Vi1qkESjQz9NBSOb9Vv" target="_blank" title="Spotify"><img alt="" src="assets/images/music/Spotify.svg" style="width:20px;height:20px;margin:10px;" title="Spotify">Spotify</a> <a href="https://music.apple.com/gb/artist/goldmaster/1498583501" target="_blank" title="Itunes"><img alt="" src="assets/images/music/Itunes.svg" style="width:20px;height:20px;margin:10px;" title="Itunes">Itunes</a> <a href="https://listen.tidal.com/artist/18321378" target="_blank" title="Tidal"><img alt="" src="assets/images/music/Tidal.svg" style="width:20px;height:20px;margin:10px;" title="Tidal">Tidal</a> <!--<a href="https://music.amazon.com/artists/B084NTWHYQ" target="_blank" title="Amazon"><img alt="" src="../Music/Amazon.svg" style="width:20px;height:20px;margin:10px;" title="Amazon">Amazon</a>--> <a href="https://DJ.Beatport.com/Goldmaster" target="_blank" title="Beatport"><img alt="" src="assets/images/music/Beatport.svg" style="width:20px;height:20px;margin:10px;" title="Beatport">Beatport</a> <a href="https://www.deezer.com/us/artist/85647332" target="_blank" title="Deezer"><img alt="" src="assets/images/music/Deezer.svg" style="width:20px;height:20px;margin:10px;" title="Deezer">Deezer</a> <a href="https://music.youtube.com/channel/UCPAEmo5eAFpLCs0ydTdPBYQ" target="_blank" title="YoutubeMusic"><img alt="" src="assets/images/music/YoutubeMusic.svg" style="width:20px;height:20px;margin:10px;" title="YoutubeMusic">Youtube Music</a> <a href="https://Goldmaster.Bandcamp.com" target="_blank" title="Bandcamp"><img alt="" src="assets/images/music/Bandcamp.svg" style="width:20px;height:20px;margin:10px;" title="Bandcamp">Bandcamp</a> <a href="https://Soundcloud.com/Goldmaster" target="_blank" title="Soundcloud"><img alt="" src="assets/images/music/Soundcloud.svg" style="width:20px;height:20px;margin:10px;" title="Soundcloud">Soundcloud</a> <a href="https://Mixcloud.com/Goldmaster" target="_blank" title="Mixcloud"><img alt="" src="assets/images/music/Mixcloud.svg" style="width:20px;height:20px;margin:10px;" title="Mixcloud">Mixcloud</a>
+			</div>
+		</div>
 
-		?>
-		<br/>
-		<button onclick="javascript:window.print()">Print Success Page</button>
+
+		<div class="socialcolumn">
+			<h3>Socials</h3>
+
+
+			<div class="externallinks">
+				<a href="https://pixelfed.social/Goldmaster" target="_blank" title="Pixelfed"><img alt="" src="assets/images/socials/Pixelfed.svg" style="width:20px;height:20px;margin:10px;" title="Pixelfed">Pixelfed</a> <!--<a href="https://www.instagram.com/Goldmaster/" target="_blank" title="Instagram"><img alt="" src="../Socials/Instagram.svg" style="width:20px;height:20px;margin:10px;" title="Instagram">Instagram</a>--> <!--<a href="https://www.minds.com/goldmaster" target="_blank" title="Minds"><img alt="" src="../Socials/Minds.svg" style="width:20px;height:20px;margin:10px;" title="Minds">Minds</a> <a href="https://www.facebook.com/Goldmaster" target="_blank" title="Facebook"><img alt="" src="../Socials/Facebook.svg" style="width:20px;height:20px;margin:10px;" title="Facebook">Facebook</a>-->
+
+				<!-- <a href="https://mastodon.social/@Goldmaster" target="_blank" title="Mastodon"><img alt="" src="assets/images/socials/Mastodon.svg" style="width:20px;height:20px;margin:10px;" title="Mastodon">Mastodon</a><a href="https://www.twitter.com/Goldmaster" target="_blank" title="Twitter"><img alt="" src="assets/images/socials/Twitter.svg" style="width:20px;height:20px;margin:10px;" title="Twitter">Twitter</a><!--<a href="https://www.reddit.com/r/Goldmaster" target="_blank" title="Reddit"> <img alt="" src="../Socials/Reddit.svg" style="width:20px;height:20px;margin:10px;" title="Reddit">Reddit</a><a href="https://www.deviantart.com/GoldmasterArt" target="_blank" title="DeviantArt"><img alt="" src="assets/images/socials/DeviantArt.svg" style="width:20px;height:20px;margin:10px;" title="DeviantArt">DeviantArt</a><a href="https://www.youtube.com/onestopviewfiles?sub_confirmation=1" target="_blank" title="Youtube"><img alt="" src="assets/images/socials/Youtube.svg" style="width:20px;height:20px;margin:10px;" title="Youtube">Youtube</a> <a href="https://www.patreon.com/Goldmaster" target="_blank" title="Patreon"><img alt="" src="assets/images/socials/Patreon.svg" style="width:20px;height:20px;margin:10px;" title="Patreon">Patreon</a> -->
+			</div>
+		</div>
+	</div>
+	<!--End of social media icons-->
+
+
+	<p>&nbsp;</p>
+	<!--Start of Base -->
+
+
+	<div class="FlexContainer">
+		<a>Made with 💧, 🥵 & 😢</a>
 	</div>
 
 
-	
+	<p style="text-align: center"><a href="#Top">Back to top</a>
+	</p>
+	<span style="text-align: center"></span>
 
 
-<?php
-get_footer();
+</body>
+</html>
